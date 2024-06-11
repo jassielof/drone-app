1c4a91c2a09f1d5ccc99eb56eddcf4b6e2fabaa8
jassielof <jassielovando@protonmail.com>
2024-06-11 19:36:39 -0400

    merge

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:dronvolador1/services/auth_service.dart';
@@ -46,24 +47,38 @@ class _HomeViewState extends State<HomeView> {
   }
 
   void _getDevices() async {
+    // Ensure Bluetooth is enabled
+    bool isEnabled = await _bluetooth.isEnabled ?? false;
+    if (!isEnabled) {
+      await _bluetooth.requestEnable();
+    }
+
+    // Get bonded devices
     var res = await _bluetooth.getBondedDevices();
     setState(() => _devices = res);
-
-    // Start discovering devices
-    _bluetooth.startDiscovery().listen((r) {
-      setState(() {
-        // Add the discovered device to the list if it's not already present
-        if (!_devices.contains(r.device)) {
-          _devices.add(r.device);
-        }
-      });
-    });
   }
 
   void _sendData(String data) {
     if (_connection?.isConnected ?? false) {
-      _connection?.output.add(ascii.encode(data));
+      print('Sending data: $data');
+      _connection?.output.add(Uint8List.fromList(utf8.encode(data + "\r\n")));
+      _connection?.output.allSent.then((_) {
+        print('Data sent');
+      }).catchError((error) {
+        print('Error sending data: $error');
+      });
+    } else {
+      print('No connection established');
     }
+  }
+
+  void _disconnect() async {
+    await _connection?.close();
+    setState(() {
+      _deviceConnected = null;
+      _connection = null;
+    });
+    print('Device disconnected');
   }
 
   final stt.SpeechToText _speechToText = stt.SpeechToText();
@@ -173,7 +188,15 @@ class _HomeViewState extends State<HomeView> {
                   onPressed: _selectDevice,
                   child: Text('Seleccionar dispositivo'),
                 )
-              : Text('Conectado a: ${_deviceConnected!.name}'),
+              : Column(
+                  children: [
+                    Text('Conectado a: ${_deviceConnected!.name}'),
+                    ElevatedButton(
+                      onPressed: _disconnect,
+                      child: Text('Desconectar'),
+                    ),
+                  ],
+                ),
           _isConnecting
               ? CircularProgressIndicator()
               : Column(
@@ -231,17 +254,31 @@ class _HomeViewState extends State<HomeView> {
                       Navigator.pop(context);
                       setState(() => _isConnecting = true);
 
-                      _connection =
-                          await BluetoothConnection.toAddress(device.address);
-                      setState(() {
-                        _deviceConnected = device;
-                        _isConnecting = false;
-                      });
+                      try {
+                        _connection =
+                            await BluetoothConnection.toAddress(device.address);
+                        setState(() {
+                          _deviceConnected = device;
+                          _isConnecting = false;
+                        });
+                        print('Connected to the device');
+                      } catch (e) {
+                        setState(() => _isConnecting = false);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('Error al conectar: $e')),
+                        );
+                      }
                     },
                   ))
               .toList(),
         );
       },
     );
+  }
+
+  @override
+  void dispose() {
+    _connection?.dispose();
+    super.dispose();
   }
 }

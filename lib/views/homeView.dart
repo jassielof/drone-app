0311284d759c3644db43d09b8c718998527925a1
jassielof <jassielovando@protonmail.com>
2024-06-11 21:07:28 -0400

    tts

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -3,6 +3,9 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:dronvolador1/services/auth_service.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -16,11 +19,31 @@ class _HomeViewState extends State<HomeView> {
   bool _isConnecting = false;
   BluetoothDevice? _deviceConnected;
   List<BluetoothDevice> _devices = [];
+  final FlutterTts _flutterTts = FlutterTts();
+  String _currentCommand = '';
+  double _voiceVolume = 0.0;
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   @override
   void initState() {
     super.initState();
     _getDevices();
+    _initializeTts();
+    _initializeNotifications();
+  }
+
+  Future<void> _initializeNotifications() async {
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('app_icon');
+    final InitializationSettings initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> _initializeTts() async {
+    await _flutterTts.awaitSpeakCompletion(true);
+    await _flutterTts.setLanguage('es-ES');
   }
 
   void _getDevices() async {
@@ -56,6 +79,90 @@ class _HomeViewState extends State<HomeView> {
       _connection = null;
     });
     print('Device disconnected');
+  }
+
+  final stt.SpeechToText _speechToText = stt.SpeechToText();
+  bool _isListening = false;
+
+  void _listenForCommand() async {
+    if (!_isListening) {
+      bool available = await _speechToText.initialize(
+        onStatus: (status) => print('onStatus: $status'),
+        onError: (val) => print('onError: $val'),
+      );
+
+      if (available) {
+        setState(() => _isListening = true);
+        _speechToText.listen(
+          onResult: (val) {
+            setState(() {
+              _currentCommand = val.recognizedWords;
+              _voiceVolume = val.confidence;
+            });
+            print('Heard: ${val.recognizedWords}');
+            _processCommand(val.recognizedWords);
+            _resetListeningState(); // Reset the listening state after processing the command
+          },
+          pauseFor: Duration(
+              seconds: 5), // Pause for 5 seconds after receiving a result
+          onSoundLevelChange: (val) {
+            setState(() => _voiceVolume = val);
+          },
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speechToText.stop();
+    }
+  }
+
+  void _resetListeningState() {
+    Future.delayed(Duration(seconds: 5), () {
+      setState(() {
+        _isListening = false;
+      });
+    });
+  }
+
+  void _processCommand(String command) {
+    switch (command.toLowerCase()) {
+      case 'power on.' || 'power on' || 'power on!':
+        print("Se envió el comando de encender a través de voz");
+        _sendData('1');
+        _showNotification('Encender motores');
+        break;
+      case 'power off.' || 'power off' || 'power off!':
+        print("Se envió el comando de apagar a través de voz");
+        _sendData('2');
+        _showNotification('Apagar motores');
+        break;
+      case 'go up.' || 'go up' || 'go up!':
+        print("Se envió el comando de subir a través de voz");
+        _sendData('3');
+        _showNotification('Subir');
+        break;
+      case 'go down.' || 'go down' || 'go down!':
+        print("Se envió el comando de bajar a través de voz");
+        _sendData('4');
+        _showNotification('Bajar');
+        break;
+      default:
+        print("Comando no reconocido");
+        break;
+    }
+  }
+
+  Future<void> _showNotification(String command) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails('your_channel_id', 'your_channel_name',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker');
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'Comando aceptado', command, platformChannelSpecifics,
+        payload: 'item x');
   }
 
   @override
@@ -109,6 +216,24 @@ class _HomeViewState extends State<HomeView> {
                     ElevatedButton(
                       onPressed: () => _sendData('4'),
                       child: Text('Bajar'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _listenForCommand,
+                      child: Column(
+                        children: [
+                          Text(_isListening
+                              ? 'Stop Listening'
+                              : 'Listen for Command'),
+                          if (_isListening)
+                            Column(
+                              children: [
+                                Text('Current Command: $_currentCommand'),
+                                Text(
+                                    'Voice Volume: ${(_voiceVolume * 100).toStringAsFixed(1)}%'),
+                              ],
+                            ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,17 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import flutter_local_notifications
+import flutter_tts
+import speech_to_text_macos
+import text_to_speech_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+  TextToSpeechMacOsPlugin.register(with: registry.registrar(forPlugin: "TextToSpeechMacOsPlugin"))
 }
